4ad81c81b9158e9053f5607f7c8a505d1307dc4a
Vincent Magnoux <vincent.magnoux@polymtl.ca>
2017-06-23 18:27:06 -0400

    - Comments

--- CudaMath.h
+++ CudaMath.h
@@ -46,6 +46,9 @@ inline __host__ __device__ float3 operator-(const float3& a, const float3& b)
     return make_float3(a.x - b.x, a.y - b.y, a.z - b.z);
 }
 
+/////////////////////
+// Vector operations
+
 inline __host__ __device__ float Dot(const float3& a, const float3& b)
 {
     return a.x * b.x + a.y * b.y + a.z * b.z;
