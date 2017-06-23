28fff4cc5871d22c09b72339c8c8a7c40e1eebad
Vincent Magnoux <vincent.magnoux@polymtl.ca>
2017-06-23 18:25:05 -0400

    - More math operations

--- CudaMath.h
+++ CudaMath.h
@@ -4,6 +4,19 @@
 #include <cuda_runtime.h>
 
 //////////////////////////
+/// float2
+/////////////////////////
+inline __host__ __device__ float Dot(const float2& a, const float2& b)
+{
+    return a.x * b.x + a.y * b.y;
+}
+
+inline __host__ __device__ float Length(const float2& Vector)
+{
+    return sqrtf(Dot(Vector, Vector));
+}
+
+//////////////////////////
 /// uint3
 /////////////////////////
 inline __host__ __device__ uint3 make_uint3(const uint a)
@@ -11,6 +24,10 @@ inline __host__ __device__ uint3 make_uint3(const uint a)
     return make_uint3(a, a, a);
 }
 
+inline __host__ __device__ uint3 operator+(const uint3& a, const uint3& b)
+{
+    return make_uint3(a.x + b.x, a.y + b.y, a.z + b.z);
+}
 
 /////////////////////////
 /// float3
@@ -19,6 +36,11 @@ inline __host__ __device__ uint3 make_uint3(const uint a)
 inline __host__ __device__ float3 abs(const float3& a)
 {
     return make_float3(fabsf(a.x), fabsf(a.y), fabsf(a.z));
+}
+
+inline __host__ __device__ float3 operator*(const float3& a, const float3& b)
+{
+    return make_float3(a.x * b.x, a.y * b.y, a.z * b.z);
 }
 
 inline __host__ __device__ float3 operator*(const float3& a, const float b)
@@ -41,9 +63,23 @@ inline __host__ __device__ float3 operator+(const float3& a, const float3& b)
     return make_float3(a.x + b.x, a.y + b.y, a.z + b.z);
 }
 
+inline __host__ __device__ void operator+=(float3& a, const float3& b)
+{
+    a.x += b.x;
+    a.y += b.y;
+    a.z += b.z;
+}
+
 inline __host__ __device__ float3 operator-(const float3& a, const float3& b)
 {
     return make_float3(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+
+inline __host__ __device__ void operator-=(float3& a, const float3& b)
+{
+    a.x -= b.x;
+    a.y -= b.y;
+    a.z -= b.z;
 }
 
 inline __host__ __device__ float Dot(const float3& a, const float3& b)
@@ -56,9 +92,25 @@ inline __host__ __device__ float Length(const float3& Vector)
     return sqrtf(Dot(Vector, Vector));
 }
 
+inline __host__ __device__ float3 Normalize(const float3& Vector)
+{
+    float Len = Length(Vector);
+    return (Vector / Len);
+}
+
+inline __host__ __device__ float3 Cross(const float3& a, const float3& b)
+{
+    return make_float3(a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x);
+}
+
 inline __host__ __device__ float3 make_float3(const float a)
 {
     return make_float3(a, a, a);
+}
+
+inline __host__ __device__ float3 make_float3(const uint3& a)
+{
+    return make_float3(a.x, a.y, a.z);
 }
 
 inline __host__ __device__ bool operator==(const float3& a, const float3& b)
